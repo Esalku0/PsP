7352948856d320d918f565ee960f8858ade16a38
ALUMNADO\esalku <esalku@floridauniversitaria.es>
2022-09-19 15:47:33 +0200

    Subiendo archivos de PsP (Primeros ejercicios)

---
+++ Act1/src/estanis/aliaga/ej1.java
@@ -0,0 +1,17 @@
+package estanis.aliaga;
+
+public class ej1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num1 =3;
+		int num2=4;
+		
+		int result = num1 + num2;
+		
+		System.out.println(result);
+		
+	}
+
+}
